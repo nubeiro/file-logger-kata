1e529b5c11cb18400263822653f2a7f3b834dc70
Raul Araya <nubeiro@gmail.com>
2017-01-27 23:37:48 +0100

    Refactor log file to class field.

--- src/Nubeiro/FileLogger.php
+++ src/Nubeiro/FileLogger.php
@@ -4,9 +4,11 @@ namespace Nubeiro;
 
 class FileLogger
 {
+    private $logFile = "file.log";
+
     public function log($message)
     {
-        $result = file_put_contents("file.log", $message);
+        $result  = file_put_contents($this->logFile, $message);
         if ($result === false) {
             return false;
         }
@@ -16,6 +18,6 @@ class FileLogger
 
     public function read()
     {
-        return file_get_contents("file.log");
+        return file_get_contents($this->logFile);
     }
 }
